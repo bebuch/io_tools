59b06e866803708f6c1745dea66568bf48597e06
Benjamin Roland Buch <benni.buch@gmail.com>
2017-11-06 16:56:07 +0100

    fix formating bugs

--- include/io_tools/mask_non_print.hpp
+++ include/io_tools/mask_non_print.hpp
@@ -20,6 +20,15 @@ namespace io_tools{
 	inline std::string mask_non_print(std::string const& str){
 		std::ostringstream result;
 		for(auto c: str){
+			if(auto d = static_cast< std::uint8_t >(c); d > 127){
+				result << "\\0x";
+				auto nipple_to_hex = [](std::uint8_t e){
+						return e > 9 ? e - 10 + 'A' : e + '0';
+					};
+				result << nipple_to_hex(d >> 4);
+				result << nipple_to_hex(d & 0x0F);
+				continue;
+			}
 			switch(c){
 				case 0: result << "\\0"; break;
 				case 1: result << "\\x01"; break;
@@ -35,24 +44,25 @@ namespace io_tools{
 				case 11: result << "\\v"; break;
 				case 12: result << "\\f"; break;
 				case 13: result << "\\r"; break;
-				case 14: result << "\\0E"; break;
-				case 15: result << "\\0F"; break;
-				case 16: result << "\\10"; break;
-				case 17: result << "\\11"; break;
-				case 18: result << "\\12"; break;
-				case 19: result << "\\13"; break;
-				case 20: result << "\\14"; break;
-				case 21: result << "\\15"; break;
-				case 22: result << "\\16"; break;
-				case 23: result << "\\17"; break;
-				case 24: result << "\\18"; break;
-				case 25: result << "\\19"; break;
-				case 26: result << "\\1A"; break;
+				case 14: result << "\\x0E"; break;
+				case 15: result << "\\x0F"; break;
+				case 16: result << "\\x10"; break;
+				case 17: result << "\\x11"; break;
+				case 18: result << "\\x12"; break;
+				case 19: result << "\\x13"; break;
+				case 20: result << "\\x14"; break;
+				case 21: result << "\\x15"; break;
+				case 22: result << "\\x16"; break;
+				case 23: result << "\\x17"; break;
+				case 24: result << "\\x18"; break;
+				case 25: result << "\\x19"; break;
+				case 26: result << "\\x1A"; break;
 				case 27: result << "\\e"; break;
-				case 28: result << "\\1C"; break;
-				case 29: result << "\\1D"; break;
-				case 30: result << "\\1E"; break;
-				case 31: result << "\\1F"; break;
+				case 28: result << "\\x1C"; break;
+				case 29: result << "\\x1D"; break;
+				case 30: result << "\\x1E"; break;
+				case 31: result << "\\x1F"; break;
+				case 127: result << "\\x7F"; break;
 				case '\\': result << "\\\\"; break;
 				default: result << c;
 			}
