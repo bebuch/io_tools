82ff7d17ff4ce5041d25bf6fb842a6f1b6853920
Benjamin Roland Buch <benni.buch@gmail.com>
2017-11-10 18:20:54 +0100

    genéralized c array output

--- include/io_tools/c_array.hpp
+++ include/io_tools/c_array.hpp
@@ -6,48 +6,34 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //-----------------------------------------------------------------------------
-#ifndef _io_tools__array_2d__hpp_INCLUDED_
-#define _io_tools__array_2d__hpp_INCLUDED_
+#ifndef _io_tools__c_array__hpp_INCLUDED_
+#define _io_tools__c_array__hpp_INCLUDED_
 
 #include <ostream>
+#include <string_view>
 
 
-namespace io_tools{ namespace array_2d{
+namespace io_tools{ namespace c_array{
 
 
 	// Use it by:
 	//
-	// using ::io_tools::array_2d::operator<<;
-
+	// using ::io_tools::c_array::operator<<;
 
 	template <
 		typename CharT, typename Traits,
-		typename T, std::size_t Y, std::size_t X >
+		typename T, std::size_t N >
 	std::basic_ostream< CharT, Traits >& operator<<(
 		std::basic_ostream< CharT, Traits >& os,
-		T(&m)[Y][X]
+		T(&m)[N]
 	){
 		os << '{';
-		for(std::size_t y = 0; y < Y; ++y){
-			if(y != 0) os << ", ";
-			os << '{';
-			for(std::size_t x = 0; x < X; ++x){
-				if(x != 0) os << ", ";
-				os << m[y][x];
-			}
-			os << '}';
+		for(std::size_t i = 0; i < N; ++i){
+			using namespace std::literals::string_view_literals;
+			if(i != 0) os << ", "sv;
+			os << m[i];
 		}
 		return os << '}';
-	}
-
-	template <
-		typename CharT, typename Traits,
-		typename T, std::size_t Y, std::size_t X >
-	std::basic_ostream< CharT, Traits >& operator<<(
-		std::basic_ostream< CharT, Traits >& os,
-		T(*m)[Y][X]
-	){
-		return os << *m;
 	}
 
 
