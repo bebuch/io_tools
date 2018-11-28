9299f5176c6075c4b83fe54b0696962e8db086b4
Benjamin Buch <benni.buch@gmail.com>
2018-11-28 17:26:30 +0100

    changed test install path to a generic location

--- script/test.sh
+++ script/test.sh
@@ -5,11 +5,13 @@ set -e
 set -o xtrace
 
 PROJECT_DIR=$(pwd)
+INSTALL_PATH=$PROJECT_DIR/../tmp/usr
+mkdir -p $INSTALL_PATH
 
 # Configure Project
 mkdir -p $PROJECT_DIR/build
 cd $PROJECT_DIR/build
-cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_DIR/usr -DIO_TOOLS_BUILD_TESTS=ON ..
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DIO_TOOLS_BUILD_TESTS=ON ..
 
 # Build test
 make
@@ -23,9 +25,9 @@ test/cpp17/cpp17tests
 make install
 
 # Check install
-git diff --no-index $PROJECT_DIR/usr/include $PROJECT_DIR/include
+git diff --no-index $INSTALL_PATH/include $PROJECT_DIR/include
 
-CMAKE_FIND_FILE=$PROJECT_DIR/usr/lib/cmake/io_tools/io_tools-config.cmake
+CMAKE_FIND_FILE=$INSTALL_PATH/lib/cmake/io_tools/io_tools-config.cmake
 if [ -a $CMAKE_FIND_FILE ]
 then
     echo "$CMAKE_FIND_FILE - Found"
@@ -37,6 +39,6 @@ fi
 # Check install by example project
 mkdir -p $PROJECT_DIR/build-package-test
 cd $PROJECT_DIR/build-package-test
-cmake -DCMAKE_PREFIX_PATH=$PROJECT_DIR/usr ../test-package
+cmake -DCMAKE_PREFIX_PATH=$INSTALL_PATH ../test-package
 make
 ./test_io_tools_package
